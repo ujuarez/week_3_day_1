d1df486f7a1fbec8b71d50ce06a9d8b0f53c1282
Juarez <ujuarez2@cps.k12.il.us>
2024-09-12 09:24:35 -0500

    Changes on 9/12/24, 9:24am

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -185,19 +185,42 @@ print("Still, " + name + " was the best coder on the team and could write " + st
 
 ##########################################################################################
 # #addition
+print(2+1)
 # #multiplication
+print(2*2)
 # #division
+print(6/2)
 # #modulo
+print(7%4) #remainder of 7 divided by 4
 # #powers
+print(2**3) #2 to the power of 3
 # #get the max and min of a number
+print("the max of 2 and 3 is", max(2,3))
+#max means the largest number
+print("the min of 2 and 3 is", min(2,3))
+#min means the lowest number
+
+
 # #round a number
+print("round 3.9 is", round(3.9))
 # # absolute value
+print("the absolute value of -3 is", abs(-3))
+#absolute value means the distance from 0
+# it will always be positive
+
+
 # # order of operations
 # #to do more you need to import special math libraries from python
-# #from math import *    
+from math import *    
 # #this goes out and grabs some different math functions we can use
 # #floor method
+print("the floor of 3.7 is",floor(3.7))
+print("the floor of 128.8 is",floor(128.8))
+#floor means round down
 # #ceil method
+print("the ceil of 3.7 is",ceil(3.7))
+print("the ceil of 128.8 is",ceil(128.8))
+#ceil means round up
 # #sqrt method
 
 
@@ -218,20 +241,37 @@ print("Still, " + name + " was the best coder on the team and could write " + st
 # # **Getting Input from users**
 # #how do we get input from users?
 # input("what is your name?")
+name = input("What is your name?")
 # # basic math calculator
 # #ask the user for 2 numbers
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
 # # print out a statement where you:
 # # add them together
+print(num1 + num2)
 # #multiply
+print(num1 * num2)
 # # find the max number
+print(max(num1, num2))
 # # find the remainder of the numbers
+print(num1 % num2)
 # #round one number
+print(round(num1))
 
 
 
+##########################################################################################
 
 
-
+numb1 = int(input("Enter your first number:"))
+numb2 = int(input("Enter your second number:"))
+print("The total when subtracting your two numbers is",(numb1 - numb2))
+print("The total when dividing your two numbers is",(numb1 / numb2))
+print("THe min of your numbers is",min(numb1,numb2))
+print("The absolute value of your first number is",abs(numb1))
+print("The floor of your first number is",floor(numb1))
+print("The ceiling of your second number is",ceil(numb2))
+print("The square root of your first number is",sqrt(numb1))
 
 
 ##########################################################################################
